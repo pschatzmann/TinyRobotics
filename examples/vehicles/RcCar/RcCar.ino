cc4031626839c67b692e0ed36250f385f97574ed
pschatzmann <phil.schatzmann@gmail.com>
2026-03-27 22:02:29 +0100

    RCGamepadMessageSource

---
+++ examples/vehicles/RcCar/RcCar.ino
@@ -0,0 +1,44 @@
+// A remote controlled car example using the RCGamepadMessageSource to receive
+// gamepad input and control the car accordingly.
+
+#include <WiFi.h>
+#include "TinyRobotics.h"
+#include "TinyRobotics/communication/RCGamepadMessageSource.h"
+
+WiFiServer server(80);
+RCGamepadMessageSource rcSource(server);
+CarAckerman car;
+const char* ssid = "your-ssid";
+const char* password = "your-password";
+
+// We use WiFi for RC control
+void startWiFi() {
+  // Connect to WiFi (replace with your network credentials)
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("RC Car example starting");
+
+  // Set up the car pins (example for 1 drive motor and 1 steering servo)
+  car.setPins(5, 6, 9, 10); // in1, in2, pwm, steeringPin
+  rcSource.addMessageHandler(car); // Connect the car to the RC message source
+
+  // Start the RC gamepad message source
+  rcSource.begin(ControlScenario::Car);
+}
+
+void loop() {
+  // Update the RC gamepad message source to handle incoming messages
+  rcSource.update();
+}
