730a4fb7dec2854cf23a7e2094b131ec89be0402
pschatzmann <phil.schatzmann@gmail.com>
2026-04-01 12:44:43 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@e37fbd3b21032068ca93d48819bd794c4076ca24 🚀

--- search/all_7.js
+++ search/all_7.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['handlequeueerror_0',['handleQueueError',['../classtinyrobotics_1_1ESPNowStream.html#a188bf9216b5c95c5ac5fae4f5631fff0',1,'tinyrobotics::ESPNowStream']]],
   ['handletransmissionresult_1',['handleTransmissionResult',['../classtinyrobotics_1_1ESPNowStream.html#a15a1ee88a9dd78ca207e5d4b0e4e6383',1,'tinyrobotics::ESPNowStream']]],
-  ['handlewaypoint_2',['handleWaypoint',['../classtinyrobotics_1_1MotionController2D.html#a8e0d6e92b882f49785eb4e7020a37939',1,'tinyrobotics::MotionController2D']]],
+  ['handlewaypoint_2',['handleWaypoint',['../classtinyrobotics_1_1MotionController2D.html#ab41934208e3c81352c4823f770f0f2b9',1,'tinyrobotics::MotionController2D']]],
   ['hash_3c_20tinyrobotics_3a_3acoordinate_3c_20t_20_3e_20_3e_3',['hash&lt; tinyrobotics::Coordinate&lt; T &gt; &gt;',['../structstd_1_1hash_3_01tinyrobotics_1_1Coordinate_3_01T_01_4_01_4.html',1,'std']]],
   ['hasobstacle_4',['hasObstacle',['../classtinyrobotics_1_1RangeSensor.html#aae8c80f0ed7b82ca0360e1342a3ac103',1,'tinyrobotics::RangeSensor']]],
   ['heading_5',['heading',['../structtinyrobotics_1_1Fusion2D_1_1State2D.html#ac5682e48513a771560df50e3b213e61a',1,'tinyrobotics::Fusion2D::State2D']]],
