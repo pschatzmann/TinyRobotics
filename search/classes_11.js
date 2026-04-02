8aa0af592e1e457536cb2f921e5a81a62cbb77ae
pschatzmann <phil.schatzmann@gmail.com>
2026-04-02 03:02:25 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@4af84bdb467034e23a92a60f40be8bfc3f8a37e3 🚀

--- search/classes_11.js
+++ search/classes_11.js
@@ -14,10 +14,9 @@ var searchData=
   ['speedfromthrottle_11',['SpeedFromThrottle',['../classtinyrobotics_1_1SpeedFromThrottle.html',1,'tinyrobotics']]],
   ['state2d_12',['State2D',['../structtinyrobotics_1_1Fusion2D_1_1State2D.html',1,'tinyrobotics::Fusion2D']]],
   ['state3d_13',['State3D',['../structtinyrobotics_1_1Fusion3D_1_1State3D.html',1,'tinyrobotics::Fusion3D']]],
-  ['stepper_14',['Stepper',['../classStepper.html',1,'']]],
-  ['steppermotor_15',['StepperMotor',['../classtinyrobotics_1_1StepperMotor.html',1,'tinyrobotics']]],
-  ['stream_16',['Stream',['../classtinyrobotics__arduino_1_1Stream.html',1,'tinyrobotics_arduino']]],
-  ['synchronizedbuffer_17',['SynchronizedBuffer',['../classtinyrobotics_1_1SynchronizedBuffer.html',1,'tinyrobotics']]],
-  ['synchronizedqueue_18',['SynchronizedQueue',['../classtinyrobotics_1_1SynchronizedQueue.html',1,'tinyrobotics']]],
-  ['synchronizedstream_19',['SynchronizedStream',['../classtinyrobotics_1_1SynchronizedStream.html',1,'tinyrobotics']]]
+  ['steppermotor_14',['StepperMotor',['../classtinyrobotics_1_1StepperMotor.html',1,'tinyrobotics']]],
+  ['stream_15',['Stream',['../classtinyrobotics__arduino_1_1Stream.html',1,'tinyrobotics_arduino']]],
+  ['synchronizedbuffer_16',['SynchronizedBuffer',['../classtinyrobotics_1_1SynchronizedBuffer.html',1,'tinyrobotics']]],
+  ['synchronizedqueue_17',['SynchronizedQueue',['../classtinyrobotics_1_1SynchronizedQueue.html',1,'tinyrobotics']]],
+  ['synchronizedstream_18',['SynchronizedStream',['../classtinyrobotics_1_1SynchronizedStream.html',1,'tinyrobotics']]]
 ];
