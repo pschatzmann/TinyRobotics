2aa0baef3b6081501193ffeb8583a685ba14d4ff
pschatzmann <phil.schatzmann@gmail.com>
2026-04-02 18:14:45 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@9e2a56553b277d730169a7fd658880559afc092d 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -1,13 +1,14 @@
 var searchData=
 [
-  ['findpath_0',['findPath',['../classtinyrobotics_1_1AStar.html#af0b94251fe8a05844a1868ef6025a70a',1,'tinyrobotics::AStar::findPath()'],['../classtinyrobotics_1_1Dijkstra.html#a2183d5d1a2f1f1ac416328c2146d65fa',1,'tinyrobotics::Dijkstra::findPath()']]],
-  ['frame2d_1',['Frame2D',['../structtinyrobotics_1_1Frame2D.html',1,'tinyrobotics']]],
-  ['frame3d_2',['Frame3D',['../classtinyrobotics_1_1Frame3D.html',1,'tinyrobotics']]],
-  ['framemgr2d_3',['FrameMgr2D',['../classtinyrobotics_1_1FrameMgr2D.html',1,'tinyrobotics']]],
-  ['framemgr3d_4',['FrameMgr3D',['../classtinyrobotics_1_1FrameMgr3D.html',1,'tinyrobotics']]],
-  ['frametype_5',['FrameType',['../group__utils.html#gae63373b1147a2e480cea6083c3f9dc6c',1,'tinyrobotics']]],
-  ['fromstring_6',['fromString',['../classtinyrobotics_1_1GPSCoordinate.html#a90a596c64056d74e218b1a77186546d4',1,'tinyrobotics::GPSCoordinate']]],
-  ['fusion_7',['Fusion',['../group__fusion.html',1,'']]],
-  ['fusion2d_8',['Fusion2D',['../classtinyrobotics_1_1Fusion2D.html#a02761b1baa1443f52b8b0de02859af08',1,'tinyrobotics::Fusion2D::Fusion2D()'],['../classtinyrobotics_1_1Fusion2D.html',1,'Fusion2D']]],
-  ['fusion3d_9',['Fusion3D',['../classtinyrobotics_1_1Fusion3D.html',1,'tinyrobotics']]]
+  ['feedforwardthrottle_0',['feedforwardThrottle',['../classtinyrobotics_1_1MotionController2D.html#aec5be575507485c0b3822d3b022f765b',1,'tinyrobotics::MotionController2D']]],
+  ['findpath_1',['findPath',['../classtinyrobotics_1_1AStar.html#af0b94251fe8a05844a1868ef6025a70a',1,'tinyrobotics::AStar::findPath()'],['../classtinyrobotics_1_1Dijkstra.html#a2183d5d1a2f1f1ac416328c2146d65fa',1,'tinyrobotics::Dijkstra::findPath()']]],
+  ['frame2d_2',['Frame2D',['../structtinyrobotics_1_1Frame2D.html',1,'tinyrobotics']]],
+  ['frame3d_3',['Frame3D',['../classtinyrobotics_1_1Frame3D.html',1,'tinyrobotics']]],
+  ['framemgr2d_4',['FrameMgr2D',['../classtinyrobotics_1_1FrameMgr2D.html',1,'tinyrobotics']]],
+  ['framemgr3d_5',['FrameMgr3D',['../classtinyrobotics_1_1FrameMgr3D.html',1,'tinyrobotics']]],
+  ['frametype_6',['FrameType',['../group__utils.html#gae63373b1147a2e480cea6083c3f9dc6c',1,'tinyrobotics']]],
+  ['fromstring_7',['fromString',['../classtinyrobotics_1_1GPSCoordinate.html#a90a596c64056d74e218b1a77186546d4',1,'tinyrobotics::GPSCoordinate']]],
+  ['fusion_8',['Fusion',['../group__fusion.html',1,'']]],
+  ['fusion2d_9',['Fusion2D',['../classtinyrobotics_1_1Fusion2D.html#a02761b1baa1443f52b8b0de02859af08',1,'tinyrobotics::Fusion2D::Fusion2D()'],['../classtinyrobotics_1_1Fusion2D.html',1,'Fusion2D']]],
+  ['fusion3d_10',['Fusion3D',['../classtinyrobotics_1_1Fusion3D.html',1,'tinyrobotics']]]
 ];
--- search/variables_d.js
+++ search/variables_d.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['timems_0',['timeMs',['../structtinyrobotics_1_1Fusion2D_1_1State2D.html#a9e7047b024dd6fbb87b3098a281326b9',1,'tinyrobotics::Fusion2D::State2D::timeMs()'],['../structtinyrobotics_1_1Fusion3D_1_1State3D.html#a9e7047b024dd6fbb87b3098a281326b9',1,'tinyrobotics::Fusion3D::State3D::timeMs()']]],
-  ['timestamp_1',['timestamp',['../classtinyrobotics_1_1SingleBuffer.html#a465bef81f6478756e5443025b1f2ddfa',1,'tinyrobotics::SingleBuffer']]],
-  ['type_2',['type',['../structtinyrobotics_1_1ParsedMessage.html#af23dd5df3133ce1a1582c66913241c0d',1,'tinyrobotics::ParsedMessage']]]
+  ['throttlemode_0',['throttleMode',['../classtinyrobotics_1_1MotionController2D.html#a7d238c24d914c3e2592c8366ef46d490',1,'tinyrobotics::MotionController2D']]],
+  ['timems_1',['timeMs',['../structtinyrobotics_1_1Fusion2D_1_1State2D.html#a9e7047b024dd6fbb87b3098a281326b9',1,'tinyrobotics::Fusion2D::State2D::timeMs()'],['../structtinyrobotics_1_1Fusion3D_1_1State3D.html#a9e7047b024dd6fbb87b3098a281326b9',1,'tinyrobotics::Fusion3D::State3D::timeMs()']]],
+  ['timestamp_2',['timestamp',['../classtinyrobotics_1_1SingleBuffer.html#a465bef81f6478756e5443025b1f2ddfa',1,'tinyrobotics::SingleBuffer']]],
+  ['type_3',['type',['../structtinyrobotics_1_1ParsedMessage.html#af23dd5df3133ce1a1582c66913241c0d',1,'tinyrobotics::ParsedMessage']]]
 ];
