f16372d343e827c48d6aba7329087f126e362519
pschatzmann <phil.schatzmann@gmail.com>
2026-04-02 03:43:06 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@cac78d0fcfd066c57ae96e109e425bf50362fdab 🚀

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['hardwareserial_0',['HardwareSerial',['../structtinyrobotics__arduino_1_1HardwareSerial.html',1,'tinyrobotics_arduino']]],
-  ['hash_3c_20tinyrobotics_3a_3acoordinate_3c_20t_20_3e_20_3e_1',['hash&lt; tinyrobotics::Coordinate&lt; T &gt; &gt;',['../structstd_1_1hash_3_01tinyrobotics_1_1Coordinate_3_01T_01_4_01_4.html',1,'std']]]
+  ['hash_3c_20tinyrobotics_3a_3acoordinate_3c_20t_20_3e_20_3e_0',['hash&lt; tinyrobotics::Coordinate&lt; T &gt; &gt;',['../structstd_1_1hash_3_01tinyrobotics_1_1Coordinate_3_01T_01_4_01_4.html',1,'std']]]
 ];
