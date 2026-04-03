f75b6f6e3c45cb3f23a01b7239a06b068ab33890
pschatzmann <phil.schatzmann@gmail.com>
2026-04-03 19:44:45 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@323e9267d50a82ba394059430d1f94763b51229a 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -8,7 +8,7 @@ var searchData=
   ['cameraobstacledetector_5',['CameraObstacleDetector',['../classtinyrobotics_1_1CameraObstacleDetector.html',1,'tinyrobotics']]],
   ['carackerman_6',['CarAckerman',['../classtinyrobotics_1_1CarAckerman.html',1,'tinyrobotics']]],
   ['cardifferential_7',['CarDifferential',['../classtinyrobotics_1_1CarDifferential.html',1,'tinyrobotics']]],
-  ['cell_8',['Cell',['../structtinyrobotics_1_1GridMap_1_1Cell.html',1,'GridMap&lt; StateType, T &gt;::Cell'],['../structtinyrobotics_1_1GridMapBitVector_1_1Cell.html',1,'GridMapBitVector::Cell']]],
+  ['cell_8',['Cell',['../structtinyrobotics_1_1GridBitMap_1_1Cell.html',1,'GridBitMap::Cell'],['../structtinyrobotics_1_1GridMap_1_1Cell.html',1,'GridMap&lt; StateType, T &gt;::Cell']]],
   ['coordinate_9',['Coordinate',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]],
   ['coordinate_3c_20distancem_20_3e_10',['Coordinate&lt; DistanceM &gt;',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]],
   ['coordinate_3c_20float_20_3e_11',['Coordinate&lt; float &gt;',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]]
--- search/classes_6.js
+++ search/classes_6.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['genericmotor_0',['GenericMotor',['../classtinyrobotics_1_1GenericMotor.html',1,'tinyrobotics']]],
   ['gpscoordinate_1',['GPSCoordinate',['../classtinyrobotics_1_1GPSCoordinate.html',1,'tinyrobotics']]],
-  ['gridmap_2',['GridMap',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]],
-  ['gridmap_3c_20cellstate_20_3e_3',['GridMap&lt; CellState &gt;',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]],
-  ['gridmapbitvector_4',['GridMapBitVector',['../classtinyrobotics_1_1GridMapBitVector.html',1,'tinyrobotics']]]
+  ['gridbitmap_2',['GridBitMap',['../classtinyrobotics_1_1GridBitMap.html',1,'tinyrobotics']]],
+  ['gridmap_3',['GridMap',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]],
+  ['gridmap_3c_20cellstate_20_3e_4',['GridMap&lt; CellState &gt;',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]],
+  ['gridmapbitvector_5',['GridMapBitVector',['../classGridMapBitVector.html',1,'']]]
 ];
