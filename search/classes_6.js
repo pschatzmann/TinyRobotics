2741ca249a5e5ae03479930771a9d954cab28bea
pschatzmann <phil.schatzmann@gmail.com>
2026-04-03 13:11:47 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@0cd8868648c14cee7a2a5a57920a8e49db4fdaa9 🚀

--- search/classes_6.js
+++ search/classes_6.js
@@ -3,5 +3,6 @@ var searchData=
   ['genericmotor_0',['GenericMotor',['../classtinyrobotics_1_1GenericMotor.html',1,'tinyrobotics']]],
   ['gpscoordinate_1',['GPSCoordinate',['../classtinyrobotics_1_1GPSCoordinate.html',1,'tinyrobotics']]],
   ['gridmap_2',['GridMap',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]],
-  ['gridmap_3c_20cellstate_20_3e_3',['GridMap&lt; CellState &gt;',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]]
+  ['gridmap_3c_20cellstate_20_3e_3',['GridMap&lt; CellState &gt;',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]],
+  ['gridmapbitvector_4',['GridMapBitVector',['../classtinyrobotics_1_1GridMapBitVector.html',1,'tinyrobotics']]]
 ];
