fa5ba0fe9d120d28658de168c0b89bad7090f85f
pschatzmann <phil.schatzmann@gmail.com>
2026-04-03 09:11:10 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@d4116d5bdc0d829529d214c2c55cc891ef366ac8 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,14 +1,15 @@
 var searchData=
 [
   ['callbackmap_0',['CallbackMap',['../classtinyrobotics_1_1CallbackMap.html',1,'tinyrobotics']]],
-  ['cameraedgefollower_1',['CameraEdgeFollower',['../classtinyrobotics_1_1CameraEdgeFollower.html',1,'tinyrobotics']]],
-  ['cameraimagediff_2',['CameraImageDiff',['../classtinyrobotics_1_1CameraImageDiff.html',1,'tinyrobotics']]],
-  ['cameralinefollower_3',['CameraLineFollower',['../classtinyrobotics_1_1CameraLineFollower.html',1,'tinyrobotics']]],
-  ['cameraobstacledetector_4',['CameraObstacleDetector',['../classtinyrobotics_1_1CameraObstacleDetector.html',1,'tinyrobotics']]],
-  ['carackerman_5',['CarAckerman',['../classtinyrobotics_1_1CarAckerman.html',1,'tinyrobotics']]],
-  ['cardifferential_6',['CarDifferential',['../classtinyrobotics_1_1CarDifferential.html',1,'tinyrobotics']]],
-  ['cell_7',['Cell',['../structtinyrobotics_1_1GridMap_1_1Cell.html',1,'tinyrobotics::GridMap']]],
-  ['coordinate_8',['Coordinate',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]],
-  ['coordinate_3c_20distancem_20_3e_9',['Coordinate&lt; DistanceM &gt;',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]],
-  ['coordinate_3c_20float_20_3e_10',['Coordinate&lt; float &gt;',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]]
+  ['callbackmessagehandler_1',['CallbackMessageHandler',['../classtinyrobotics_1_1CallbackMessageHandler.html',1,'tinyrobotics']]],
+  ['cameraedgefollower_2',['CameraEdgeFollower',['../classtinyrobotics_1_1CameraEdgeFollower.html',1,'tinyrobotics']]],
+  ['cameraimagediff_3',['CameraImageDiff',['../classtinyrobotics_1_1CameraImageDiff.html',1,'tinyrobotics']]],
+  ['cameralinefollower_4',['CameraLineFollower',['../classtinyrobotics_1_1CameraLineFollower.html',1,'tinyrobotics']]],
+  ['cameraobstacledetector_5',['CameraObstacleDetector',['../classtinyrobotics_1_1CameraObstacleDetector.html',1,'tinyrobotics']]],
+  ['carackerman_6',['CarAckerman',['../classtinyrobotics_1_1CarAckerman.html',1,'tinyrobotics']]],
+  ['cardifferential_7',['CarDifferential',['../classtinyrobotics_1_1CarDifferential.html',1,'tinyrobotics']]],
+  ['cell_8',['Cell',['../structtinyrobotics_1_1GridMap_1_1Cell.html',1,'tinyrobotics::GridMap']]],
+  ['coordinate_9',['Coordinate',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]],
+  ['coordinate_3c_20distancem_20_3e_10',['Coordinate&lt; DistanceM &gt;',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]],
+  ['coordinate_3c_20float_20_3e_11',['Coordinate&lt; float &gt;',['../classtinyrobotics_1_1Coordinate.html',1,'tinyrobotics']]]
 ];
