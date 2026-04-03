82be17a889ffb7ec461d0e0b1d27b5b19a75d0d1
pschatzmann <phil.schatzmann@gmail.com>
2026-04-03 01:54:43 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@f8ca064da544c6e409df35ee5935c03c955f0685 🚀

--- search/classes_10.js
+++ search/classes_10.js
@@ -5,7 +5,7 @@ var searchData=
   ['rcgamepadmessagesource_2',['RCGamepadMessageSource',['../classtinyrobotics_1_1RCGamepadMessageSource.html',1,'tinyrobotics']]],
   ['readioheadstream_3',['ReadioHeadStream',['../classtinyrobotics_1_1ReadioHeadStream.html',1,'tinyrobotics']]],
   ['rebind_4',['rebind',['../structtinyrobotics_1_1AllocatorPSRAM_1_1rebind.html',1,'tinyrobotics::AllocatorPSRAM']]],
-  ['result_5',['Result',['../structtinyrobotics_1_1CameraEdgeFollower_1_1Result.html',1,'CameraEdgeFollower::Result'],['../structtinyrobotics_1_1CameraLineFollower_1_1Result.html',1,'CameraLineFollower::Result'],['../structtinyrobotics_1_1CameraObstacleDetector_1_1Result.html',1,'CameraObstacleDetector::Result']]],
+  ['result_5',['Result',['../structtinyrobotics_1_1CameraEdgeFollower_1_1Result.html',1,'CameraEdgeFollower::Result'],['../structtinyrobotics_1_1CameraLineFollower_1_1Result.html',1,'CameraLineFollower::Result'],['../structtinyrobotics_1_1CameraObstacleDetector_1_1Result.html',1,'CameraObstacleDetector::Result'],['../structtinyrobotics_1_1PIDAutoTuner_1_1Result.html',1,'PIDAutoTuner::Result']]],
   ['ringbuffer_6',['RingBuffer',['../classtinyrobotics_1_1RingBuffer.html',1,'tinyrobotics']]],
   ['ringbuffer_3c_20uint8_5ft_20_3e_7',['RingBuffer&lt; uint8_t &gt;',['../classtinyrobotics_1_1RingBuffer.html',1,'tinyrobotics']]]
 ];
