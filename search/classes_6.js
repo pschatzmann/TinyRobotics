3ed03dedf8e3f2c7229ac4f0290e2b2bacf02bde
pschatzmann <phil.schatzmann@gmail.com>
2026-04-06 08:02:41 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@e2fa5bb459fd67a9fb16a946c66d373e70d523c8 🚀

--- search/classes_6.js
+++ search/classes_6.js
@@ -4,8 +4,7 @@ var searchData=
   ['gpscoordinate_1',['GPSCoordinate',['../classtinyrobotics_1_1GPSCoordinate.html',1,'tinyrobotics']]],
   ['gridbitmap_2',['GridBitMap',['../classtinyrobotics_1_1GridBitMap.html',1,'tinyrobotics']]],
   ['gridmap_3',['GridMap',['../classtinyrobotics_1_1GridMap.html',1,'tinyrobotics']]],
-  ['gridmapbitvector_4',['GridMapBitVector',['../classGridMapBitVector.html',1,'']]],
-  ['gridmapserializer_5',['GridMapSerializer',['../classtinyrobotics_1_1GridMapSerializer.html',1,'tinyrobotics']]],
-  ['gridmapserializer_3c_20tinyrobotics_3a_3agridbitmap_2c_20cellstate_2c_20distancem_20_3e_6',['GridMapSerializer&lt; tinyrobotics::GridBitMap, CellState, DistanceM &gt;',['../classtinyrobotics_1_1GridMapSerializer.html',1,'tinyrobotics']]],
-  ['gridmapserializer_3c_20tinyrobotics_3a_3agridmap_3c_20cellstate_20_3e_2c_20cellstate_2c_20distancem_20_3e_7',['GridMapSerializer&lt; tinyrobotics::GridMap&lt; CellState &gt;, CellState, DistanceM &gt;',['../classtinyrobotics_1_1GridMapSerializer.html',1,'tinyrobotics']]]
+  ['gridmapserializer_4',['GridMapSerializer',['../classtinyrobotics_1_1GridMapSerializer.html',1,'tinyrobotics']]],
+  ['gridmapserializer_3c_20tinyrobotics_3a_3agridbitmap_2c_20cellstate_2c_20distancem_20_3e_5',['GridMapSerializer&lt; tinyrobotics::GridBitMap, CellState, DistanceM &gt;',['../classtinyrobotics_1_1GridMapSerializer.html',1,'tinyrobotics']]],
+  ['gridmapserializer_3c_20tinyrobotics_3a_3agridmap_3c_20cellstate_20_3e_2c_20cellstate_2c_20distancem_20_3e_6',['GridMapSerializer&lt; tinyrobotics::GridMap&lt; CellState &gt;, CellState, DistanceM &gt;',['../classtinyrobotics_1_1GridMapSerializer.html',1,'tinyrobotics']]]
 ];
