be6b09e06ff12aed7b1e272e1baa1ceaeab2ef98
pschatzmann <phil.schatzmann@gmail.com>
2026-04-06 15:10:42 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@1dc27935de28885ba5c1c0b2b0692e2886701abd 🚀

--- search/variables_9.js
+++ search/variables_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['oui_0',['oui',['../structtinyrobotics_1_1ESPNowStreamConfig.html#aba6724ce067b3e9c93b0f13c0e0a3f49',1,'tinyrobotics::ESPNowStreamConfig']]]
+  ['origin_0',['origin',['../structtinyrobotics_1_1Message.html#ac90a3dca2fb51712ee5c8657c50facc0',1,'tinyrobotics::Message']]],
+  ['oui_1',['oui',['../structtinyrobotics_1_1ESPNowStreamConfig.html#aba6724ce067b3e9c93b0f13c0e0a3f49',1,'tinyrobotics::ESPNowStreamConfig']]]
 ];
