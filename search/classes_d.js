352a1876fe36e9914ab9b914064c8f222e248b9c
pschatzmann <phil.schatzmann@gmail.com>
2026-04-08 09:07:18 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@ab5b26954db752f02281a4823339a1749a11ba9d 🚀

--- search/classes_d.js
+++ search/classes_d.js
@@ -4,7 +4,6 @@ var searchData=
   ['odometry3d_1',['Odometry3D',['../classtinyrobotics_1_1Odometry3D.html',1,'tinyrobotics']]],
   ['odometrydifferentialdrivemodel_2',['OdometryDifferentialDriveModel',['../classtinyrobotics_1_1OdometryDifferentialDriveModel.html',1,'tinyrobotics']]],
   ['odometrymodel2d_3',['OdometryModel2D',['../classtinyrobotics_1_1OdometryModel2D.html',1,'tinyrobotics']]],
-  ['odometrymodelackerman_4',['OdometryModelAckerman',['../classOdometryModelAckerman.html',1,'']]],
-  ['optional_5',['optional',['../classstd_1_1optional.html',1,'std']]],
-  ['orientation3d_6',['Orientation3D',['../classtinyrobotics_1_1Orientation3D.html',1,'tinyrobotics']]]
+  ['optional_4',['optional',['../classstd_1_1optional.html',1,'std']]],
+  ['orientation3d_5',['Orientation3D',['../classtinyrobotics_1_1Orientation3D.html',1,'tinyrobotics']]]
 ];
