911df207adeabccf8886a50a69f962ed7df73670
pschatzmann <phil.schatzmann@gmail.com>
2026-04-14 16:36:19 +0000

    Deploying to doxygen from @ pschatzmann/TinyRobotics@9b306af7e874a925856f30b122d8fa0053377bd4 🚀

--- search/enums_1.js
+++ search/enums_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['cellstate_0',['CellState',['../group__utils.html#ga8787c656e38c35a93bccad5fe9a8d7d9',1,'tinyrobotics']]]
+  ['cellstate_0',['CellState',['../group__utils.html#ga8787c656e38c35a93bccad5fe9a8d7d9',1,'tinyrobotics']]],
+  ['controlscenario_1',['ControlScenario',['../group__utils.html#ga54e0752930cfaf3caf6bde26a0f14595',1,'tinyrobotics']]]
 ];
